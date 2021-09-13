5b6bfbd81c6614310ef426816845d150a1199095
sakurapath <leandro.canestraro050@gmail.com>
2021-09-13 20:19:46 -0300

    files

---
+++ src/test/java/br/com/jabes/ponto/PontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jabes.ponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
